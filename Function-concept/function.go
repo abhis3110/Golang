1165ca33c6209b13d36428057d2bf88eb01b760e
Abhishek shukla <abhishekshukla824@gmail.com>
2023-06-14 00:35:11 +0530

    fun-return-two-value

--- Function-concept/function.go
+++ Function-concept/function.go
@@ -6,7 +6,18 @@ func sum(a int, b int) int {
 	return a + b
 }
 
+func ops(a int, b int) (int, int) {
+	return a + b, a - b
+
+}
+
 func main() {
 	result := sum(2, 2)
 	fmt.Println(result)
+
+	// function may return multiple values
+	sum, subs := ops(2, 2)
+	fmt.Println("2+2=", sum, "2-2=", subs)
+	b, _ := ops(10, 2)
+	fmt.Println("10+2=", b)
 }
